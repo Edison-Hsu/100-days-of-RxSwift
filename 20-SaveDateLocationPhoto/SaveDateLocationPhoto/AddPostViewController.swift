241431abcce16ceabac414400999ef6e09168f6b
Edison-Hsu <edison.hsu.sh@gmail.com>
2016-10-08 02:48:18 +0800

    update swift 3.0

---
+++ 20-SaveDateLocationPhoto/SaveDateLocationPhoto/AddPostViewController.swift
@@ -0,0 +1,67 @@
+//
+//  AddPostViewController.swift
+//  TimelineExercises
+//
+//  Created by 徐 栋栋 on 5/14/16.
+//  Copyright © 2016 EdisonHsu. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import CoreLocation
+
+class AddPostViewController: UIViewController {
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    fileprivate let disposeBag = DisposeBag()
+    
+    lazy var toolbar: UIToolbar = {
+       var bar = UIToolbar(frame: CGRect(x: 0,y: 0,width: self.view.frame.width, height: 44))
+        bar.tintColor = UIColor.white
+        bar.isTranslucent = false
+        bar.clipsToBounds = true
+        bar.items = [self.cameraButton, self.locationButton]
+        return bar
+    }()
+    
+    let cameraButton: UIBarButtonItem = {
+        let btn = UIBarButtonItem()
+        btn.image = UIImage(named: "camera")!.withRenderingMode(.alwaysOriginal)
+        btn.tintColor = UIColor.init(colorLiteralRed: 239/255, green: 249/255, blue: 246/255, alpha: 1.0)
+        return btn
+    }()
+    
+    let locationButton: UIBarButtonItem = {
+        let btn = UIBarButtonItem()
+        btn.image = UIImage(named: "location")!.withRenderingMode(.alwaysOriginal)
+        return btn
+    }()
+    
+    let locationManager = CLLocationManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        textView.inputAccessoryView = toolbar
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 20-SaveDateLocationPhoto/SaveDateLocationPhoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveDateLocationPhoto
+//
+//  Created by 徐 栋栋 on 08/10/2016.
+//  Copyright © 2016 EdisonHsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

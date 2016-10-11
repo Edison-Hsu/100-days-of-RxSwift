63278618cc54f50a8f28d66704633d21e6538d09
Edison-Hsu <edison.hsu.sh@gmail.com>
2016-10-11 23:13:48 +0800

    finish basic tableView

--- 20-SaveDateLocationPhoto/SaveDateLocationPhoto/ViewController.swift
+++ 20-SaveDateLocationPhoto/SaveDateLocationPhoto/ViewController.swift
@@ -7,12 +7,48 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+
+class Item {
+//    private let image:UIImageView
+//    private let date:NSDate
+//    private let content:String
+    let location:String
+    
+    init(location:String) {
+//    init(image: UIImageView, date: NSDate, content: String, location: String) {
+//        self.image = image
+//        self.date = date
+//        self.content = content
+        self.location = location
+    }
+}
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    let items = Variable(Array<Item>())
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        items.value = [
+            Item(location: "123"),
+            Item(location: "223")
+        ]
+//        let items = Observable.just([
+//            "First Item",
+//            "Second Item",
+//            "Third Item"
+//            ])
+        
+        items.asObservable()
+            .bindTo(tableView.rx.items(cellIdentifier: "Cell", cellType: UITableViewCell.self)) { (row, element, cell) in
+                cell.textLabel?.text = "\(element.location) @ row \(row)"
+            }
+            .addDisposableTo(disposeBag)
+        
     }
 
     override func didReceiveMemoryWarning() {
